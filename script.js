7ac50c68e985a930cce33cbfc04024373da203dc
RAYMOND PETERS <raymondpeters117@gmail.com>
2026-07-17 19:13:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -249,4 +249,25 @@ ticker.innerHTML =
 "No new announcements";
 
 
-};
+}
+const slides = document.querySelectorAll(".slide");
+
+let current = 0;
+
+function nextSlide(){
+
+slides[current].classList.remove("active");
+
+current++;
+
+if(current >= slides.length){
+
+current = 0;
+
+}
+
+slides[current].classList.add("active");
+
+}
+
+setInterval(nextSlide,5000);;
